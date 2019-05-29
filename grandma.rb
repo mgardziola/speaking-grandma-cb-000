a1789e17040e2a39cd64254b819dcf06cd20fed7
mariah <mariah@gardziola.com>
2019-05-29 16:34:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,13 +15,13 @@
 def speak_to_grandma(phrase)
   
   if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
     
    elsif phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   
   else 
-    puts "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   end
   
 end
