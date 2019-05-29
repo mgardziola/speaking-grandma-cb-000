57af4428b43f57c8a11309c65b7d9264d091605b
mariah <mariah@gardziola.com>
2019-05-29 16:27:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,13 +14,13 @@
 
 def speak_to_grandma(phrase)
   
-  if phrase == phrase.downcase
+  if phrase == phrase.upcase
     puts "HUH?! SPEAK UP, SONNY!"
     
    elsif phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   
-  else phrase
+  else 
     puts "NO, NOT SINCE 1938!"
   end
   
