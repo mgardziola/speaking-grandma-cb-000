01219e4b037a8f96c1b4c8ff8c355fbf7e1d7d9b
mariah <mariah@gardziola.com>
2019-05-29 16:26:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,13 +15,13 @@
 def speak_to_grandma(phrase)
   
   if phrase == phrase.downcase
-    puts 'HUH?! SPEAK UP, SONNY!'
+    puts "HUH?! SPEAK UP, SONNY!"
     
-   elsif phrase == 'I LOVE YOU GRANDMA!'
-    puts'I LOVE YOU TOO PUMPKIN!'
+   elsif phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
   
   else phrase
-    puts 'NO, NOT SINCE 1938!'
+    puts "NO, NOT SINCE 1938!"
   end
   
 end
